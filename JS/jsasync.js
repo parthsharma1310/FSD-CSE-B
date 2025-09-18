aedef4eba65cd0885579f4c7939445ebbf83c37a
Miscreantboy <parthh.ssharmafacebook@gmail.com>
2025-09-18 15:12:14 +0530

    Async js

---
+++ JS/jsasync.js
@@ -0,0 +1,36 @@
+function register(){
+    setTimeout(()=>{
+        console.log("register end");
+        
+    },2000)
+}
+function SendEmail(){
+    setTimeout(()=>{
+        console.log("Send Email");
+        
+    },2000)
+}
+function Login(){
+    setTimeout(()=>{
+        console.log("Log in");
+        
+    },2000)
+}
+function getdata(){
+    setTimeout(()=>{
+        console.log("getdata");
+        
+    },2000)
+}
+function display(){
+    setTimeout(()=>{
+        console.log("display data");
+        
+    },2000)
+}
+register();
+SendEmail();
+Login();
+getdata();
+display();
+console.log("Call other application");
